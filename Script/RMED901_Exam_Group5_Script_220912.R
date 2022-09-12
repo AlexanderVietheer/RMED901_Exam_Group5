4dc225eb4695326b9c900271ee6b6953ecbb92d5
AlexanderVietheer <alexander.vietheer@mailbox.org>
2022-09-12 11:50:18 +0200

    exploringthe dataset 1st  steps

--- Script/RMED901_Exam_Group5_Script_220912.R
+++ Script/RMED901_Exam_Group5_Script_220912.R
@@ -8,6 +8,22 @@ library(tidyverse)
 library(here)
 
 #read inn the dataset
-
 myData <- read_csv(here("DATA", "exam_nontidy.txt"))
+
+# exploring the data set
+head(myData)
+
+# we have to read inn the dataset sensitive for tabulator delimitor
+myData <- read_delim(here("DATA", "exam_nontidy.txt"), delim = "\t")
+
+head(myData) 
+## this looks better
+
+# exploring the data and have a look at all variables in rows; basic summary stats
+skimr::skim(myData)
+## we have 1214 rows; 31 columns
+
+
+
+
 
